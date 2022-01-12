737d3cd54f42289bec2cbe9a13c980eb4bb5dbc5
megmon <june.milkywa.d@gmail.com>
2022-01-12 22:42:15 +0900

    環境設定＆ログイン画面の作成
    ※application.propertiesの内容はDB作成後に修正が必要。

---
+++ Day8HW/src/main/java/com/example/Day8HwApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day8HwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day8HwApplication.class, args);
+	}
+
+}
