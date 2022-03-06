db5e5dd7bac92768a959f88dcbfe567d2c62c403
megmon <june.milkywa.d@gmail.com>
2022-03-06 22:59:05 +0900

    認可処理修正（ロールの先頭にROLE_を追加）

--- Day8HW/src/main/java/com/example/config/SecurityConfig.java
+++ Day8HW/src/main/java/com/example/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.authorizeHttpRequests()
 				.antMatchers("/login").permitAll() //直リンクOK
 				.antMatchers("/user/signup").permitAll() //直リンクOK
-				.antMatchers("/admin").hasAuthority("ADMIN")//権限制御
+				.antMatchers("/admin").hasAuthority("ROLE_ADMIN")//権限制御
 				.anyRequest().authenticated(); //それ以外は直リンクNG
 		
 		//ログイン処理
